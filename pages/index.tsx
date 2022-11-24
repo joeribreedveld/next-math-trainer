53869768a0c7ea2f42a67a37c9003a2749415b37
Duck1s <joeri.breedveld123@gmail.com>
2022-11-24 10:44:07 +0100

    feat(answer): numberlimit state, handlechange, check answer and make input field

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ const Home: NextPage = () => {
   // State
   const [answer, setAnswer] = useState(0);
   const [question, setQuestion] = useState("");
+  const [numberLimit, setNumberLimit] = useState(10);
 
   // Functions
   // Generate operator
@@ -29,8 +30,8 @@ const Home: NextPage = () => {
   // Generate question
   const generateQuestion = () => {
     // Generate random numbers
-    const num1 = Math.floor(Math.random() * 10);
-    const num2 = Math.floor(Math.random() * 10);
+    const num1 = Math.floor(Math.random() * numberLimit);
+    const num2 = Math.floor(Math.random() * numberLimit);
 
     // Generate operator
     const operator = generateOperator();
@@ -58,6 +59,25 @@ const Home: NextPage = () => {
     setQuestion(question);
   };
 
+  // Handle change
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Get value
+    const value = e.target.value;
+
+    // Check if value is answer
+    if (value == answer.toString()) {
+      // Handle correct answer
+      // Log correct
+      console.log("Correct!");
+
+      // Generate new question
+      generateQuestion();
+
+      // Clear input
+      e.target.value = "";
+    }
+  };
+
   // useEffect
   useEffect(() => {
     // Generate question
@@ -67,6 +87,9 @@ const Home: NextPage = () => {
   return (
     <>
       <h1>{question}</h1>
+      <form>
+        <input type="number" onChange={handleChange} name="answer" />
+      </form>
     </>
   );
 };
