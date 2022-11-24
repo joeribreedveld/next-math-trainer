3c85f84e0b7c98be1f871c2d272ac36987ebcb84
Duck1s <joeri.breedveld123@gmail.com>
2022-11-24 10:11:32 +0100

    clean: remove all unused files and add basic comments

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Imports
+import type { NextApiRequest, NextApiResponse } from "next";
 
+// Types
 type Data = {
-  name: string
+  name: string;
+};
+
+// Functions
+function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.status(200).json({ name: "John Doe" });
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// Exports
+export default handler;
