3f29138639b2004af71b9bb7b8c1c30253e0462f
Duck1s <joeri.breedveld123@gmail.com>
2022-11-24 10:19:03 +0100

    feat(question): generate operator function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,30 @@
 // Imports
 import type { NextPage } from "next";
+import { useState } from "react";
 
 // Functions
 const Home: NextPage = () => {
+  // State
+  const [answer, setAnswer] = useState(0);
+
+  // Functions
+  // Generate operator
+  const generateOperator = () => {
+    // Generate random number
+    const num = Math.floor(Math.random() * 4);
+    // Choose operator
+    switch (num) {
+      case 0:
+        return "+";
+      case 1:
+        return "-";
+      case 2:
+        return "*";
+      case 3:
+        return "/";
+    }
+  };
+
   return (
     <>
       <h1>Hello, world!</h1>
